bddb72518cb527911faa0428fb1d68c8721782e7
Gowtham <gowthamkumar1611@gmail.com>
2025-04-23 14:22:49 +0530

    new line added

--- src/files/admin/admin.service.ts
+++ src/files/admin/admin.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class AdminService {}
+export class AdminService {
+    constructor(
+        // @InjectRepository(Admin)
+        // private adminRepository: Repository<Admin>,
+    ) {}
+}
