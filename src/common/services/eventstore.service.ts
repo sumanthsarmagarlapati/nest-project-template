92b4faae5603da2eb1e468ada637aaef4251c19e
Gowtham <gowthamkumar1611@gmail.com>
2025-07-24 15:47:40 +0530

    Rabbitmq service added

--- src/common/services/eventstore.service.ts
+++ src/common/services/eventstore.service.ts
@@ -1,10 +1,13 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import * as amqp from 'amqplib';
+import { env } from 'process';
+
 @Injectable()
 export class EventStoreService implements OnModuleInit {
-  private connection: amqp.Connection;
-  private channel: amqp.Channel;
+  private publisherChannel: amqp.Channel;
+  private consumerChannel: amqp.Channel;
+  private isConnected = false;
   constructor(private readonly configService: ConfigService) {}
 
   onModuleInit() {
@@ -12,23 +15,56 @@ export class EventStoreService implements OnModuleInit {
   }
 
   async initService() {
-    const rabbitmqConfig = this.configService.getOrThrow('RABBIT_MQ');
+    try {
+      this.isConnected = true;
+      const rabbitmqConfig = this.configService.getOrThrow('RABBIT_MQ');
 
-    this.connection = await amqp.connect(rabbitmqConfig);
+      const connection: amqp.Connection = await amqp.connect(rabbitmqConfig);
 
-    this.connection.on('error', (err) => {
-      console.error('RabbitMQ connection error:', err);
-    });
+      connection.on('error', (err) => {
+        this.isConnected = false;
+        console.error('RabbitMQ connection error:', err);
+      });
 
-    this.channel = await this.connection.createChannel();
+      this.publisherChannel = await connection.createChannel();
+      connection.on('connect', () => {
+        console.log('RabbitMQ connection established Successful');
+      });
 
-    this.channel.on('error', (err) => {
-      console.error('RabbitMQ channel error:', err);
-    });
+      this.activateConsumer(env.RABBIT_MQ_CONSUMER);
+    } catch (error) {
+      await this.retryConnection();
+    }
+  }
 
-    this.connection.on('connect', () => {
-      console.log('RabbitMQ connection established Successful');
-    });
+  async retryConnection() {
+    if (!this.isConnected) {
+      setTimeout(async () => {
+        await this.initService();
+        console.log('Retrying RabbitMQ connection...');
+      }, 5000);
+    }
+  }
+
+  async activateConsumer(queue: string) {
+    try {
+      await this.consumerChannel.preFetch(5);
+      await this.consumerChannel.assertQueue(queue, { durable: true });
+      await this.consumerChannel.assertExchange(
+        env.EVENT_STAORE_EXCHANGE,
+        'fanout',
+        { durable: true },
+      );
+      await this.consumerChannel.bindQueue(
+        queue,
+        env.EVENT_STAORE_EXCHANGE,
+        '',
+      );
+      this.consumerChannel.consume(queue, (msg) => {
+        const mesage = JSON.parse(msg.content.toString());
+        console.log('Received message:', mesage);
+      });
+    } catch (error) {}
   }
 
   async publishMessage(
@@ -40,14 +76,23 @@ export class EventStoreService implements OnModuleInit {
     },
     message: any,
   ) {
+    const messageBuffer = Buffer.from(JSON.stringify(message));
     if (config.type === 'direct' && config?.queue) {
-    } else if (config.type == 'fanout' && config?.exchange) {
+      await this.publisherChannel.assertQueue(config.queue, { durable: true });
+      await this.publisherChannel.sendToQueue(config.queue, messageBuffer);
+    } else if (
+      (config.type == 'fanout' || config.type == 'topic') &&
+      config?.exchange
+    ) {
+      await this.publisherChannel.assertExchange(config.exchange, config.type);
+      await this.publisherChannel.publish(
+        config.exchange,
+        config.key,
+        messageBuffer,
+      );
     } else if (config.type == 'topic' && config?.exchange) {
-    } else if (config.type == 'headers' && config?.exchange) {
-    } else {
-      throw new Error('Invalid configuration');
     }
-
+    console.log('Published message:', message);
     try {
     } catch (error) {}
   }
