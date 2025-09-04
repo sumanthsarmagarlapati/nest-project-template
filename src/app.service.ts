dc88c20f8919f244fc6f86df45318334197bd74e
Sumanth Sarma <sumanthsarma1306@gmail.com>
2025-09-04 08:00:14 +0530

    Chnmage

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from "@nestjs/common";
 @Injectable()
 export class AppService {
   getHello(): string {
-    return "Say Hello World Modified!";
+    return "Say Hello World!";
   }
 }
